f3e2292600a6ea4e4a24de5be35e4f7b9bf596d7
Kate Pond <kate@thepondsedge.com>
2018-02-09 11:28:13 -0800

    adds SolarSystem Class to Wave 2

--- wave2.rb
+++ wave2.rb
@@ -21,6 +21,35 @@ class Planet
   end
 end
 
+class SolarSystem
+
+  attr_accessor :planets
+
+  def initialize(planets = Array.new)
+    @planets = planets
+  end
+
+  # method to add new planets
+  def add_new_planet(new_planet)
+    @planets.push(new_planet)
+  end
+
+  # method that returns a list of strings
+  def list_planets
+    list = ""
+    @planets.each do |planet|
+      list += "#{@planets.index(planet) + 1}. #{planet}\n"
+    end
+    return list
+  end
+end
+
 kate = Planet.new("Something", 45, "6789mi", "789days", "Cone Heads")
 
+pond = Planet.new("Pond Planet", 5, "69mi", "7days", "None")
+
 puts kate.list_attributes
+
+kate_system = SolarSystem.new(planet= [kate, pond])
+
+puts kate_system.list_planets
