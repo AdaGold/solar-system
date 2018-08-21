d5d58b49b333990cd2db8ffe4dddd146fe304aa0
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-21 14:30:32 -0700

    Passes Wave 1 tests

--- lib/main.rb
+++ lib/main.rb
@@ -4,12 +4,15 @@ require_relative "planet"
 def main
   gorgonzola = Planet.new("Gorgonzola", "yellow", 2.986e24, 1.682e10, "Only planet accesible by the Space-Q-L8R space escalator")
   scamble_dimp_iv = Planet.new("Scamble-Dimp IV", "gross bleu", 3.142e31, 4.001e10, "Worst planet in sector")
+  puts "\n", gorgonzola.summary
+  puts "\n", scamble_dimp_iv.summary, "\n"
 
-  puts "\n"
-  puts gorgonzola.summary
-  puts "\n"
-  puts scamble_dimp_iv.summary
-  puts "\n"
+  return scamble_dimp_iv
 end
 
 main
+
+
+puts main.color
+b_6_critney = Planet.new("B-6-Critney", "clear", 1.234e4, 678e9, "Made of jello")
+puts b_6_critney.color
