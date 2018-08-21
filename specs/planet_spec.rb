004b856f5d46731f17c2e5bf1b8652ea583e4b78
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-21 13:55:26 -0700

    Added tests in planet_spec; fix ln 15-16 in planet.rb to check data types

--- specs/planet_spec.rb
+++ specs/planet_spec.rb
@@ -1,1 +1,42 @@
 # /specs/planet_spec.rb
+gem 'minitest', '>= 5.0.0'
+require 'minitest/pride'
+require 'minitest/autorun'
+require "minitest/skip_dsl"
+
+require "main"
+require "planet"
+
+describe "Test Planet class" do
+  # first test that initialization works
+  it "will read initialized variables correctly" do
+    gorgonzola = Planet.new("Gorgonzola", "yellow", 2.986e24, 1.682e10, "Only planet accesible by the Space-Q-L8R space escalator")
+    expect(gorgonzola.fun_fact).must_equal "Only planet accesible by the Space-Q-L8R space escalator"
+  end
+
+  it "will allow writing, but not reading, of initialized variables" do
+    gorgonzola = Planet.new("Gorgonzola", "yellow", 2.986e24, 1.682e10, "Only planet accesible by the Space-Q-L8R space escalator")
+    expect{gorgonzola.color = "green"}.must_raise NoMethodError
+  end
+
+  xit "will raise ArgumentError for invalid mass and distance" do
+    expect{planet = Planet.new("Name", "color", "99", "99", "Fun fact")}.must_be_silent
+    expect{planet = Planet.new("Name", "color", 99, "99", "Fun fact")}.must_raise ArgumentError
+    expect{planet = Planet.new("Name", "color", "99", 99, "Fun fact")}.must_raise ArgumentError
+    expect{planet = Planet.new("Name", "color", "99", "99", "Fun fact")}.must_raise ArgumentError
+    expect{planet = Planet.new("Name", "color", 99, 0, "Fun fact")}.must_raise ArgumentError
+    expect{planet = Planet.new("Name", "color", 0, 99, "Fun fact")}.must_raise ArgumentError
+    expect{planet = Planet.new("Name", "color", 0, 0, "Fun fact")}.must_raise ArgumentError
+  end
+
+  it "will return a single string summary" do
+    gorgonzola = Planet.new("Gorgonzola", "yellow", 2.986e24, 1.682e10, "Only planet accesible by the Space-Q-L8R space escalator")
+    expect(gorgonzola.summary).must_be_instance_of String
+    expect(gorgonzola.summary).must_include "yellow"
+  end
+end
+
+# describe "Test main.rb" do
+#   it "" do
+#   end
+# end
