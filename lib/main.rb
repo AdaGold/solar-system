ca70ac2845d2b3a2ac5b65045a2f3116c52f3f9c
Ngoc Le <ngocle@outlook.com>
2019-02-27 00:09:02 -0800

    Added plant details option -part 2 of wave 3 and add planet option - part 3 of wave 3

--- lib/main.rb
+++ lib/main.rb
@@ -2,32 +2,50 @@ require_relative "planet.rb"
 require_relative "solar_system.rb"
 
 def main
+
+  #Added planets' info to each planet instance
   solar_system = SolarSystem.new("Sun")
-
-  mercury = Planet.new("Mercury", "dark grey", 4.867e24, 5.791e7, "Mercury is the smallest plannet in our solar system")
-  solar_system.add_planet(mercury)
-
-  saturn = Planet.new("Saturn", "yellowish-brown", 5.683e26, 1.434e9, "Saturn is the second largest planet in our solar system")
-  solar_system.add_planet(saturn)
 
   earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
   solar_system.add_planet(earth)
 
-  list = solar_system.list_planets
+  mercury = Planet.new("Mercury", "dark grey", 4.867e24, 5.791e7, "Mercury is the smallest plannet in our solar system")
+  solar_system.add_planet(mercury)
 
-  puts list
+  neptune = Planet.new("Neptune", "purple", 1.024e26, 4.495e9, "It is four times wider than Earth")
+  solar_system.add_planet(neptune)
 
-  found_planet = solar_system.find_planet_by_name("sATURN")
-  puts found_planet
-  puts found_planet.summary
-end
+  saturn = Planet.new("Saturn", "yellowish-brown", 5.683e26, 1.434e9, "Saturn is the second largest planet in our solar system")
+  solar_system.add_planet(saturn)
 
-puts "Welcome to Solar System program!"
-puts "Please enter 'list planets' if you would like to see facts about planets. Enter 'exit' to exit the program"
-user_input = gets.chomp
+  venus = Planet.new("Venus", "opal", 4.867e24, 1.082e8, "It has intense heat and volcanic activity")
+  solar_system.add_planet(venus)
 
-until user_input.downcase == "exit"
-  main
-  puts "Please enter 'list planets' if you would like to see facts about planets. Enter 'exit' to exit the program"
+  ######## Prompt User's inputs########
+
+  puts "Welcome to Solar System program!"
+  puts "What would you like to do?"
+  puts "[list planets] [planet details] [add planet] [exit]"
   user_input = gets.chomp
+
+  until user_input.downcase == "exit"
+    case user_input.downcase
+    when "list planets"
+      puts solar_system.list_planets
+    when "planet details"
+      puts "Which planet would you like to learn about?"
+      planet_details_input = gets.chomp
+      found_planet = solar_system.find_planet_by_name(planet_details_input.downcase)
+      puts found_planet.summary
+    when "add planet"
+      solar_system.user_added_planet
+      puts solar_system.list_planets
+    end
+
+    puts "What would you like to do next?"
+    puts "[list planets] [planet details] [add planet] [exit]"
+    user_input = gets.chomp
+  end
 end
+
+main
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -27,4 +27,21 @@ class SolarSystem
   def find_planet_by_name(planet_name)
     @planets.find { |planet| planet.name.downcase == planet_name.downcase }
   end
+
+  def user_added_planet
+    puts "Please enter new planet's name"
+    planet_name = gets.chomp
+    puts "Please enter new planet's color"
+    planet_color = gets.chomp
+    puts "Please enter new planet's mass in kilograms"
+    planet_mass = gets.chomp
+    puts "Please enter the distance between this new planet and the sun"
+    planet_distance = gets.chomp
+    puts "Please enter new planet's fun_fact"
+    fun_fact = gets.chomp
+
+    user_planet = Planet.new(planet_name.capitalize, planet_color, planet_mass, planet_distance, fun_fact)
+
+    add_planet(user_planet)
+  end
 end
