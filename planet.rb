e767a370a9f4845afdd14a646987882ee2658d42
Olga Patrakova <olgapatrakova@outlook.com>
2020-02-18 22:21:31 -0800

    Shortened summary and mass with distance validation stand before initializing instance variables

--- planet.rb
+++ planet.rb
@@ -4,20 +4,16 @@ class Planet
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
+    if mass_kg < 0 || distance_from_sun_km < 0
+      raise ArgumentError, "Mass and distance from the Sun can't be less than zero"
+    end
     @mass_kg = mass_kg
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
-
-    if @mass_kg < 0 || @distance_from_sun_km < 0
-      raise ArgumentError, "Mass and distance from the Sun can't be less than zero"
-    end
   end
 
   def summary
-    return "--------
-This planet is called #{@name}. 
-It's colored #{@color}, and it weights #{@mass_kg} kilos.
-It's #{@distance_from_sun_km} km away from the Sun, and it's the #{@fun_fact.downcase}.
---------"
+    return "The planet #{@name} is #{@color}, and it weights #{@mass_kg} kilos."\
+    " It's #{@distance_from_sun_km} km away from the Sun, and it's #{@fun_fact.downcase}."
   end
 end
