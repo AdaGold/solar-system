a67a873cb76f45788bf54b60641726292a2e1f4e
Madeline <madelign@gmail.com>
2020-09-23 00:32:37 -0700

    finished wave 1

---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative planet.rb
+
+def main
+    earth = Planet.new("Earth", "blue", 5.972e24, 1.496e8, "it's the only planet known to support life")
+    
+    mars = Planet.new("Mars", "red", 6.39e23, 2.27.496e8, "it would take more than six Mars to fill the volume of Earth")
+
+
+end
+
+
+
+main
---
+++ planet.rb
@@ -0,0 +1,19 @@
+class Planet
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+
+end
+
+  def summary
+    return "#{@name} is a #{@color} planet that weighs #{@mass_kg} kg. It's #{@distance_from_sun_km} km from the sun. Did you know that #{@fun_fact}?"
+  end 
+
+
+
+end
