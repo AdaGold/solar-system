b10eb8253b712f121744062dc7a352f15d9e5cfa
KayKay-git <kayalew@yahoo.com>
2020-09-22 21:19:31 -0400

    Wave 2 complete

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,26 @@
 require_relative 'planet.rb'
+require_relative 'solar_system'
+
 
 def main
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  p earth.summary
+  mercury = Planet.new('Mercury', 'dark-grey',2.9772e23, 1.295e8, 'The closest planet to the sun')
+  venus = Planet.new('Venus', 'white',3.9772e23, 1.395e8, 'The second closest planet to the sun')
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'The only planet known to support life')
+  mars = Planet.new('Mars', 'red', 6.972e24, 2.496e8, 'We may live there one day')
+
+  solar_system = SolarSystem.new('Solar')
+
+  solar_system.add_planet(mercury)
+  solar_system.add_planet(venus)
+  solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
+
+  list = solar_system.list_planets
+  p list
+
+  found_planet = solar_system.find_planet_by_name('Mercury')
+  p found_planet.summary
+
 end
 
 main
---
+++ lib/solar_system.rb
@@ -0,0 +1,32 @@
+require_relative 'planet.rb'
+# Wave 2
+class SolarSystem
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet_instance)
+    @planets.push(planet_instance)
+  end
+
+  def list_planets
+    i = 1
+    list = "Planets orbiting #{@star_name}:\n"
+    @planets.each do |planet|
+      list += "#{i}. #{planet.name}\n"
+      i += 1
+    end
+    return list
+  end
+
+  def find_planet_by_name(planet_name)
+    planet_found = @planets.find do |planet|
+      planet.name.capitalize == planet_name.capitalize
+    end
+    return planet_found
+  end
+
+end
